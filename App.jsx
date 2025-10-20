3540f0ba323270996b10604bf045fcbaecf4d2ed
Jy'Mere Williams <jwilli74@terpmail.umd.edu>
2025-10-20 19:30:11 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StartPage from 'StartPage'
+import StartPage from './components/StartPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<StartPage />} />
+      </Routes>
+    </BrowserRouter>
+    );
+}
+
+export default App;
+
